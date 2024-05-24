c1dbb2b6853bca072f5e3352cf87b9583d8fbfa2
Oleg Laptop <olegandreev1997@mail.ru>
2024-05-24 14:44:33 +0300

    Fill with content

--- components/Section/Footer.vue
+++ components/Section/Footer.vue
@@ -1,13 +1,36 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <div class='footer'>
-    Footer
+  <div class="footer">
+    <p class="copyright-text">
+      Copyright © 2024
+      <a href="https://www.linkedin.com/in/olegandreev/" target="_blank"
+        >Fermata Soft</a
+      >. Все права защищены.
+    </p>
   </div>
 </template>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+.copyright-text {
+  font-size: 1.4rem;
+  font-weight: 100;
+  color: $on-surface;
+  text-align: center;
+  margin-top: 50px;
+  padding-bottom: 10px;
 
+  @include breakpoint(sm) {
+    font-size: 1.2rem;
+  }
+
+  a {
+    color: $on-surface;
+    font-weight: 500;
+
+    &:hover {
+      color: $primary;
+    }
+  }
+}
 </style>
